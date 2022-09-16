33fbb001c65d6037536b144cd2eef530a6f3b62d
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-16 10:47:00 -0300

    feat(backend): add User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please, provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    require: [true, 'Please, provide an email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please, provide a valid email'
+    }
+    
+  },
+  password: {
+    type: String,
+    require: [true, 'Please, provide a password'],
+    maxlength: 30,
+    minlength: 8
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
