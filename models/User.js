864e5106ec1269b08440f653eeb01ed6f40248e6
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-16 17:13:18 -0300

    feat: add logic to hash password and compare passwords in User model

--- models/User.js
+++ models/User.js
@@ -31,4 +31,15 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+UserSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashPassword;
+})
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+}
+
 module.exports = mongoose.model('User', UserSchema);
