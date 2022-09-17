d52ceec7a1e4031f81ca0cf8d7e4e47099fbc331
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-17 13:49:36 -0300

    feat(login.js): implement login.js logic

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,9 +1,30 @@
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../../errors');
+const { attachCookiesToResponse } = require('../../utils');
+const User = require('../../models/User');
 
 async function login(req, res){
-  return res.status(StatusCodes.OK).json({
-    message: 'Login route'
-  })
+
+  const { email, password } = req.body;
+  if(!email || !password){
+    throw new BadRequestError('Please, provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  if(!user){
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  const isPasswordCorrect =  await user.comparePassword(password);
+  if(!isPasswordCorrect){
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  const tokenUser = { name: user.name, id: user._id, role: user.role };
+
+  attachCookiesToResponse({ res, user: tokenUser })
+
+  return res.status(StatusCodes.OK).json({ user: tokenUser })
 }
 
 module.exports = login;
